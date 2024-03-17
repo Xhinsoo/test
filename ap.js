d9b27253be089b7a203effb30a13796ac6792e21
LordNinth <rr621801@gmail.com>
2024-03-17 12:30:56 +0000

    test

--- ap.js
+++ ap.js
@@ -3,8 +3,7 @@ const menu = document.querySelector("ul");
 
 toggle.addEventListener("click", (e) => {
   e.preventDefault();
-  menu.classList.length>1
+  menu.classList.length > 1
     ? menu.classList.remove("displayMenu")
     : menu.classList.add("displayMenu");
-
 });
